8964b38f81599c7f4eb06f4cff84844adfb8cf2b
saurabhsantosh <saurabh@xurmo.com>
2011-07-20 13:53:21 +0530

    added communication

--- src/Cordinator.java
+++ src/Cordinator.java
@@ -1,14 +1,41 @@
 import java.net.ServerSocket;
 
 
-public class Cordinator {
+public class Cordinator implements Runnable{
 	
-	public static String ipaddress;
 	public static Integer portno;
 	
 	public static ServerSocket listenSocket;
-	public static Communication[] mainobject;
+	public static Communication[] commobject;
 	
 	Thread[] communicationthread;
+	
+	public void InitServer()
+	{
+		int i=0;
+		
+		portno=12345;
+		
+		try
+		{   
+			
+			listenSocket=new ServerSocket(portno);
+		    
+			
+			commobject[i]=new Communication();
+			
+		}
+		catch(Exception e)
+		{
+			//Showmessage("Connection Terminated");
+			return;
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
