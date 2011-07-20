3a6de46d3356871a586b1eacf488d81c58892a50
saurabhsantosh <saurabh@xurmo.com>
2011-07-20 13:13:28 +0530

    added src

---
+++ src/Cordinator.java
@@ -0,0 +1,14 @@
+import java.net.ServerSocket;
+
+
+public class Cordinator {
+	
+	public static String ipaddress;
+	public static Integer portno;
+	
+	public static ServerSocket listenSocket;
+	public static Communication[] mainobject;
+	
+	Thread[] communicationthread;
+
+}
