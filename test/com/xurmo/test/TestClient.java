bf025fda276f34dcf3e741f2366d730ef331cf8e
saurabhsantosh <saurabh@xurmo.com>
2011-07-21 09:44:03 +0530

    added code to test

--- test/com/xurmo/test/TestClient.java
+++ test/com/xurmo/test/TestClient.java
@@ -2,6 +2,7 @@ package com.xurmo.test;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -10,51 +11,50 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class TestClient {
-	
+
 	Socket clientSocket;
-    String ip;
-    int port;
-	
-	
-	public TestClient(int port,String ip){
-		
-		this.ip=ip;
-		this.port=port;
-		
+	DataOutputStream out;
+	BufferedReader in;
+	String ip;
+	int port;
+
+	public TestClient(int port, String ip) {
+
+		this.ip = ip;
+		this.port = port;
+
 	}
-	
-	public void connect()
-	{
+
+	public void connect() {
 		String message = "Hello";
-		
-		try
-		{
-		clientSocket = new Socket(ip, port);
-		writeToSocket(clientSocket, message);
-		readFromSocket(clientSocket);
-		clientSocket.close();
-		}
-		catch(Exception e)
-		{
+
+		try {
+			clientSocket = new Socket(ip, port);
+			in = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+			out = new DataOutputStream(this.clientSocket.getOutputStream());
+			writeToSocket(clientSocket, message);
+			readFromSocket(clientSocket);
+			in.close();
+			out.close();
+			clientSocket.close();
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 	}
-	
+
 	private void writeToSocket(Socket sock, String message) throws IOException {
-		PrintWriter out = new PrintWriter(new BufferedWriter(
-											new OutputStreamWriter(sock.getOutputStream())), true);
-		out.println(message);
-		out.close();
+
+		out.writeBytes(message + "\n");
+
 	}
 
 	private void readFromSocket(Socket sock) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
 		String message = in.readLine();
 		System.out.println("Server says : " + message);
-		in.close();
+
 	}
-	
-	
 
 }
